92fed4f5437c472ea9a90e4dcf61ae8959320965
Grey Kim <starbux630@naver.com>
2023-03-29 16:15:46 +0900

    hey

--- src/context/Main.jsx
+++ src/context/Main.jsx
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 import ThemeContext from "./ThemeContext";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    background: #f1f1f1;
+    color: ${props => props.theme === 'light' ? 'blue' : 'red' };
+    display: flex;
+    justify-content: space-around;
+`
 
 function Main(props) {
     //ThemeContext 참조하도록 정의
     const { theme, toggleTheme } = useContext(ThemeContext);
 
     return (
-        <div style={{
-            backgroundColor: theme === 'light' ? '#fff' : '#000',
-            color: theme === 'light' ? '#000' : '#fff'
-        }}>
+        <Wrapper theme={theme}>
             <h3>Main</h3>
             <button onClick={toggleTheme}>Toggle Theme</button>
-        </div>
+        </Wrapper>
     )
 }
 
